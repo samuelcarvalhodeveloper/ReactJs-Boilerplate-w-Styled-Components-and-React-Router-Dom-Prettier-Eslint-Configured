c9df3aaa9472aa90ec82b47776373873794c0470
Samuel Carvalho <personal.samuelcarvalho@gmail.com>
2022-10-21 18:19:50 -0300

    ReactJs-BoilerPlate

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import MainStyle from "./Styled";
+
+function Main() {
+  return (
+    <MainStyle>
+      <h1>React</h1>
+      <h2>Boilerplate</h2>
+    </MainStyle>
+  );
+}
+
+export default Main;
