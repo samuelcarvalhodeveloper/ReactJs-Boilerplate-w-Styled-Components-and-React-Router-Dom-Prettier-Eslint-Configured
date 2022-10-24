7e79c69e697ca349d403ed58c8f93e74eefe3f34
Samuel de Carvalho <93744509+samueldecarvalhodev@users.noreply.github.com>
2022-10-23 21:18:03 -0300

    official template

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,8 +3,8 @@ import MainStyle from "./Styled";
 function Main() {
   return (
     <MainStyle>
-      <h1>React</h1>
-      <h2>Boilerplate</h2>
+      <h2>React</h2>
+      <h1>Template</h1>
     </MainStyle>
   );
 }
--- src/components/Main/Styled.js
+++ src/components/Main/Styled.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
-const Main = styled.div``;
+const Main = styled.section`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #e8e5dd;
+  color: #10100e;
+
+  h1 {
+    background: #ffbd00;
+    font-size: 4rem;
+  }
+
+  h2 {
+    font-size: 3.2rem;
+    color: #232220;
+  }
+`;
 
 export default Main;
